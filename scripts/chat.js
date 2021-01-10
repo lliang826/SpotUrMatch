84cdf0c3d5ea1c8bd4689aa31d76471bc668af8b
Bryce Daynard <brycedaynard@live.com>
2021-01-10 03:03:22 -0800

    user can see who they've matched with and click on different profiles

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,14 +1,8 @@
-const msgScreen = document.getElementById("messages")
-const msgForm = document.getElementById("messageForm");
-const msgInput = document.getElementById("msg-input");
-const msgBtn = document.getElementById("msg-btn");
+const matches = document.getElementById("connectionsList");
 
-//var usermessages = db.collection("msgs").doc();
-// db.collection('resources').doc(resourceType).update({
-//    visitCount: firebase.firestore.FieldValue.increment(1)
+/* 
 
-
-/* Create a function that reads the user's name from Firebase
+Create a function that reads the user's name from Firebase
 let name="";
 function foo() {
     
@@ -17,13 +11,16 @@ function foo() {
 window.onload = function () {
     console.log("loaded");
     firebase.auth().onAuthStateChanged(function (user) {
-        db.collection("users").doc(user.uid).get().then(function(doc) {
-            var groups = doc.data().group;
-            console.log(groups);
-            for (var i = 0; i < groups.length; i++) {
+        db.collection("users").doc(user.uid).onSnapshot(function (snap) {
+            for(var i = 0; i < snap.data().group.length; i++) {
+                var newAnchor = document.createElement('a');
+                newAnchor.href = "#";
                 var newDiv = document.createElement('div');
-                newDiv.addClass('userMatches');
-                newDiv.id = doc.data().group[i];
+                newDiv.id = snap.data().group[i];
+                newDiv.classList.add('userMatches');
+                newAnchor.append(newDiv);
+                matches.append(newAnchor);
+                userInfoPull(snap.data().group[i], snap.data().username);
             }
         })
     })
@@ -31,7 +28,26 @@ window.onload = function () {
 }
 
 
+function userInfoPull(groupID, username) {
+    db.collection('users').where('group', 'array-contains', groupID)
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            if (doc.data().username != username) {
+                var name = document.createElement('h3');
+                name.innerText = doc.data().username;
+                document.getElementById(groupID).append(name);
+            }
+        })
+    })
+}
 
+
+
+function placeholder() {
+    document.getElementById("chatbox").value = "";
+}
+/*
 function addMessage() {
     message = document.getElementById("msg-input").value;
 
@@ -49,13 +65,13 @@ function clearText() {
     document.getElementById("msg-input").value = "";
 }
 
-/*window.onbeforeunload = messageClear;
+window.onbeforeunload = messageClear;
 
 function messageClear() {
     db.collection("msgs").doc("chatTest").update({
         messages: firebase.firestore.
     })
-}*/
+}
 
 
 
@@ -64,3 +80,4 @@ function messagePull() {
     $(msgScreen).empty();
 
 }
+*/

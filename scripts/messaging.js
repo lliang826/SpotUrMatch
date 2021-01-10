bcc73426fc2d55648105f967010dd1c232cbac7f
Lawrence Liang <[xlawrence.liangx@gmail.com]>
2021-01-09 19:18:06 -0800

    started the messaging app

--- scripts/messaging.js
+++ scripts/messaging.js
@@ -1,8 +1,34 @@
-const msgScreen = document.getElementById(##########);
-const msgForm = document.getElementById(########);
-const msgInput = document.getElementById(######);
-const msgBtn = document.getElementById(#######);
+const msgScreen = document.getElementById("messages")
+const msgForm = document.getElementById("messageForm");
+const msgInput = document.getElementById("msg-input");
+const msgBtn = document.getElementById("msg-btn");
 
 const db = firebase.database();
 const msgRef = db.ref("/msgs");
 
+var usermessages = db.collection("msgs").doc();
+// db.collection('resources').doc(resourceType).update({
+//    visitCount: firebase.firestore.FieldValue.increment(1)
+
+
+/* Create a function that reads the user's name from Firebase
+let name="";
+function foo() {
+    
+}*/
+
+msgForm.addEventListener('submit', sendMessage);
+
+function sendMessage(e) {
+    e.preventDefault();
+    const text = msgInput.value;
+
+    if (!text.trim()) return alert('Please type a message'); //no msg submitted
+    const msg = {
+        name: name,
+        text: text
+    };
+
+    msgRef.push(msg);
+    msgInput.value = "";
+}

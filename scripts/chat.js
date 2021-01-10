52b7703e946888d62fb174afd3682e68776d48cd
Bryce Daynard <brycedaynard@live.com>
2021-01-10 06:57:23 -0800

    messaging tweaks

--- scripts/chat.js
+++ scripts/chat.js
@@ -16,16 +16,11 @@ window.onload = function () {
                 var newAnchor = document.createElement('a');
                 newAnchor.href = "#";
                 var newDiv = document.createElement('div');
-<<<<<<< HEAD
                 newDiv.id = snap.data().group[i];
                 newDiv.classList.add('userMatches');
                 newAnchor.append(newDiv);
                 matches.append(newAnchor);
                 userInfoPull(snap.data().group[i], snap.data().username);
-=======
-                newDiv.addClass('userMatches');
-                newDiv.id = groups[i];
->>>>>>> 9ad1d9ccabb61dec0d6b3c043a1fdd6f734595c2
             }
         })
     })

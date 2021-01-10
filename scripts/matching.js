3aee3ee3b92f05a16593151d8b34087d55314ce1
Angela Qi <angela.qity@gmail.com>
2021-01-09 21:36:45 -0800

    fixed up Spotify widget.

--- scripts/matching.js
+++ scripts/matching.js
@@ -8,83 +8,88 @@ const artist3List = [];
 const recommended1List = [];
 
 function queueUser(doc) {
-    usernameList.push(doc.data().username);
-    artist1List.push(doc.data().artist1);
+  usernameList.push(doc.data().username);
+  artist1List.push(doc.data().artist1);
 
-    if (doc.get("artist2") != null) {
-        artist2List.push(doc.data().artist2);
-    } else {
-        artist2List.push(null);
-    }
+  if (doc.get("artist2") != null) {
+    artist2List.push(doc.data().artist2);
+  } else {
+    artist2List.push(null);
+  }
 
-    if (doc.get("artist3") != null) {
-        artist3List.push(doc.data().artist3);
-    } else {
-        artist3List.push(null);
-    }
+  if (doc.get("artist3") != null) {
+    artist3List.push(doc.data().artist3);
+  } else {
+    artist3List.push(null);
+  }
 
-    if (doc.get("recommended1") != null) {
-      var userRecommended1 = doc.data().recommended1;
+  if (doc.get("recommended1") != null) {
+    var userRecommended1 = doc.data().recommended1;
 
-      for (stringLength = 0; stringLength < userRecommended1.length - 2; stringLength++) {
-        
-        if (userRecommended1.charAt(stringLength) == "c"
-        && userRecommended1.charAt(stringLength + 1) == "o"
-        && userRecommended1.charAt(stringLength + 2) == "m") {
-          
-          var newEmbed = userRecommended1.substring(0, stringLength + 4) + "embed/" + userRecommended1.substring(stringLength + 4);
+    for (stringLength = 0; stringLength < userRecommended1.length - 2; stringLength++) {
 
-          recommended1List.push(newEmbed);
-          console.log("Array reccomended: " + recommended1List[i]);
-        } else {
-          recommended1List.push(null);
-        }
+      if (userRecommended1.charAt(stringLength) == "c" &&
+        userRecommended1.charAt(stringLength + 1) == "o" &&
+        userRecommended1.charAt(stringLength + 2) == "m") {
+
+        var newEmbed = userRecommended1.substring(0, stringLength + 4) + "embed/" + userRecommended1.substring(stringLength + 4);
+
+        recommended1List.push(newEmbed);
       }
     }
+  } else {
+    recommended1List.push(null);
+  }
 }
 
 function updateUser() {
-    $("#username").html(usernameList[i]);
-    $("#artist1").html(artist1List[i]);
-    $("#artist2").html(artist2List[i]);
-    $("#artist3").html(artist3List[i]);
-    //document.getElementById("spotify-replacement").src = recommended1List[i];
-    console.log(recommended1List[i]);
+  $("#username").html(usernameList[i]);
+  $("#artist1").html(artist1List[i]);
+  $("#artist2").html(artist2List[i]);
+  $("#artist3").html(artist3List[i]);
+
+  if (recommended1List[i] != null) {
+    $("#no-recommended").html("This user recommends:");
+    document.getElementById("spotify-replacement").src = recommended1List[i];
+  } else {
+    $("#no-recommended").html("This user does not recommend anything.");
+    document.getElementById("spotify-replacement").src = "";
+  }
 }
 
 // Sends each document in the "users" collection where the queries match to render().
 db.collection("users").where("artist1", "==", "smash mouth").get().then(snap => {
-    snap.forEach(doc => {
-        queueUser(doc);
-    });
-    checkIfButtonDisable();
-    updateUser();
+  snap.forEach(doc => {
+    queueUser(doc);
+  });
+  checkIfButtonDisable();
+  updateUser();
 });
 
 // "Next" button handler.
 $("#buttonRight").on("click", event => {
-    i++;
-    checkIfButtonDisable();
-    updateUser();
+  i++;
+  checkIfButtonDisable();
+  updateUser();
 });
 
 // "Previous" button handler.
 $("#buttonLeft").on("click", event => {
-    i--;
-    checkIfButtonDisable();
-    updateUser();
+  i--;
+  checkIfButtonDisable();
+  updateUser();
 });
 
 // Disables the "next" or "previous" button if a user doesn't exist in that direction.
 function checkIfButtonDisable() {
-    if (i > 0) {
-        document.getElementById("buttonLeft").disabled = false;
-    } else {
-        document.getElementById("buttonLeft").disabled = true;
-    }
-    if (i != usernameList.length - 1) {
-        document.getElementById("buttonRight").disabled = false;
-    } else {
-        document.getElementById("buttonRight").disabled = true;
-    }
+  if (i > 0) {
+    document.getElementById("buttonLeft").disabled = false;
+  } else {
+    document.getElementById("buttonLeft").disabled = true;
+  }
+  if (i != usernameList.length - 1) {
+    document.getElementById("buttonRight").disabled = false;
+  } else {
+    document.getElementById("buttonRight").disabled = true;
+  }
 }

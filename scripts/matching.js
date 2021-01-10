d6e719ef4ac1a228f36626ee16b13f8c8b1370cc
Joshua Krauchi <jkrauchi@my.bcit.ca>
2021-01-09 19:47:14 -0800

    Matching now pulls values dynamically

--- scripts/matching.js
+++ scripts/matching.js
@@ -1,13 +1,70 @@
-const usernames = [];
+// The index of the current user. This keeps track of which user is currently being displayed on the page.
+var i = 0;
 
-function render(doc) {
-    usernames.push(doc.data().username);
-    $("#username").append(doc.data().username);
+const usernameList = [];
+const artist1List = [];
+const artist2List = [];
+const artist3List = [];
+
+function queueUser(doc) {
+
+    usernameList.push(doc.data().username);
+    artist1List.push(doc.data().artist1);
+
+    if (doc.get("artist2") != null) {
+        artist2List.push(doc.data().artist2);
+    } else {
+        artist2List.push(null);
+    }
+
+    if (doc.get("artist3") != null) {
+        artist3List.push(doc.data().artist3);
+    } else {
+        artist3List.push(null);
+    }
+
+}
+
+function updateUser() {
+    $("#username").html(usernameList[i]);
+    $("#artist1").html(artist1List[i]);
+    $("#artist2").html(artist2List[i]);
+    $("#artist3").html(artist3List[i]);
 }
 
 // Sends each document in the "users" collection where the queries match to render().
 db.collection("users").where("artist1", "==", "smash mouth").get().then(snap => {
-  snap.forEach(doc => {
-    render(doc);
-  });
+    snap.forEach(doc => {
+        queueUser(doc);
+    });
+    checkIfButtonDisable();
+    updateUser();
 });
+
+// "Next" button handler.
+$("#buttonRight").on("click", event => {
+    i++;
+    checkIfButtonDisable();
+    updateUser();
+});
+
+// "Previous" button handler.
+$("#buttonLeft").on("click", event => {
+    i--;
+    checkIfButtonDisable();
+    updateUser();
+});
+
+// Disables the "next" or "previous" button if a user doesn't exist in that direction.
+function checkIfButtonDisable() {
+    if (i > 0) {
+        document.getElementById("buttonLeft").disabled = false;
+    } else {
+        document.getElementById("buttonLeft").disabled = true;
+    }
+    if (i != usernameList.length - 1) {
+        document.getElementById("buttonRight").disabled = false;
+    } else {
+        document.getElementById("buttonRight").disabled = true;
+    }
+}

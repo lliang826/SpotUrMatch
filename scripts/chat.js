ca7195e77fa7412bd6d823a623bde70438c9d025
Joshua Krauchi <jkrauchi@my.bcit.ca>
2021-01-10 07:17:55 -0800

    Added chat functionality

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,70 +1,95 @@
 const matches = document.getElementById("connectionsList");
 
-/*
+var currentGroup;
 
-Create a function that reads the user's name from Firebase
-let name="";
-function foo() {
+// If logged in, gets the currently logged in user and sets it to a variable.
+var userLoggedIn;
+auth.onAuthStateChanged(token => {
+    if (token) {
+        userLoggedIn = token;
+    }
+    renderContacts();
+});
 
-}*/
-
-window.onload = function () {
+function renderContacts() {
     console.log("loaded");
-    firebase.auth().onAuthStateChanged(function (user) {
-        db.collection("users").doc(user.uid).onSnapshot(function (snap) {
-            for(var i = 0; i < snap.data().group.length; i++) {
-                var newAnchor = document.createElement('a');
-                newAnchor.href = "#";
-                var newDiv = document.createElement('div');
-                newDiv.id = snap.data().group[i];
-                newDiv.classList.add('userMatches');
-                newAnchor.append(newDiv);
-                matches.append(newAnchor);
-                userInfoPull(snap.data().group[i], snap.data().username);
-                newDiv.addClass('userMatches');
-                newDiv.id = groups[i];
-            }
-        })
+
+    db.collection("users").doc(userLoggedIn.uid).get().then(function(snap) {
+        for (var i = 0; i < snap.data().group.length; i++) {
+            let contactAnchor = document.createElement('a');
+            contactAnchor.href = "#";
+            let contactDiv = document.createElement('div');
+            contactDiv.id = snap.data().group[i];
+            contactDiv.classList.add('userMatches');
+            contactAnchor.append(contactDiv);
+            matches.append(contactAnchor);
+
+            userInfoPull(snap.data().group[i], snap.data().username);
+
+            document.getElementById(contactDiv.id).onclick = event => {
+                $("#messages").html("");
+                console.log(contactDiv.id);
+                currentGroup = contactDiv.id;
+                renderChat(contactDiv.id);
+            };
+        }
     })
+
     console.log("complete");
 }
 
 
 function userInfoPull(groupID, username) {
     db.collection('users').where('group', 'array-contains', groupID)
-    .get()
-    .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            if (doc.data().username != username) {
-                var name = document.createElement('h3');
-                name.innerText = doc.data().username;
-                document.getElementById(groupID).append(name);
-            }
+        .get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                if (doc.data().username != username) {
+                    var name = document.createElement('h3');
+                    name.innerText = doc.data().username;
+                    document.getElementById(groupID).append(name);
+                }
+            })
         })
-    })
 }
 
-
-
-function placeholder() {
-    document.getElementById("chatbox").value = "";
-}
-/*
-function addMessage() {
-    message = document.getElementById("msg-input").value;
-
-    db.collection("msgs").doc("chatTest").update({
-        messages: firebase.firestore.FieldValue.arrayUnion(message)
+function renderChat(groupID) {
+    console.log(groupID);
+    db.collection("groups").doc(groupID).collection("msgs").get().then(snap => {
+        snap.forEach(doc => {
+            console.log("here!");
+            $("#messages").append("<li>" + doc.data().msg + "</li>");
+        });
     });
-    clearText();
-}
+};
 
+$("#submit").on("click", event => {
+    message = document.getElementById("chatbox").value;
+
+    const increment = firebase.firestore.FieldValue.increment(1);
+    db.collection("groups").doc(currentGroup).update({
+        msgNo: increment
+    }).then(event => {
+        db.collection("groups").doc(currentGroup).get().then(snap => {
+            var messageNo = snap.data().msgNo;
+            db.collection("groups").doc(currentGroup).collection("msgs").doc("" + messageNo).set({
+                msg: message,
+                sender: userLoggedIn.uid
+            });
+        });
+    });
+
+    $("#messages").append("<li>" + message + "</li>");
+    document.getElementById("chatbox").value = "";
+});
+
+/*
 msgBtn.onclick = function () {
     addMessage();
 }
 
 function clearText() {
-    document.getElementById("msg-input").value = "";
+
 }
 
 window.onbeforeunload = messageClear;

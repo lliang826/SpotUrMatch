e10c379b74f3789052476900810c225bf2e61e04
Bryce Daynard <brycedaynard@live.com>
2021-01-09 17:32:20 -0800

    messaging js start

---
+++ scripts/messaging.js
@@ -0,0 +1,8 @@
+const msgScreen = document.getElementById(##########);
+const msgForm = document.getElementById(########);
+const msgInput = document.getElementById(######);
+const msgBtn = document.getElementById(#######);
+
+const db = firebase.database();
+const msgRef = db.ref("/msgs");
+
